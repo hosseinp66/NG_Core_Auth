5ac060d1204df89c66012f296b862d33f9a16884
hp <hp@DESKTOP-JN0HRJB>
2019-05-16 12:55:29 +0430

    First commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NG_Core_Auth.Helpers;
+using NG_Core_Auth.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Text;
+
+namespace NG_Core_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController:Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        private readonly SignInManager<IdentityUser> _signManager;
+
+        private readonly AppSettings _appSetting;
+
+        public AccountController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager,
+            IOptions<AppSettings> appSettings)
+        {
+            _userManager = userManager;
+            _signManager = signInManager;
+
+            //IOption Declaration -> For access data in class
+            _appSetting = appSettings.Value;
+        }
+        
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel formdata)
+        {
+            // will hold all the errors related to registration
+            List<string> errorList = new List<string>();
+
+            var user = new IdentityUser
+            {
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = formdata.Username,
+                Email = formdata.Email                
+            };
+
+            var result = await _userManager.CreateAsync(user, formdata.Password);
+
+            if(result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+
+                // Sending Confirmation Email
+                return Ok(new { username = user.UserName, email = user.Email, status = 1, message = "Registeration Successful!" });
+            }   
+            else
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    errorList.Add(error.Description);
+                }
+            }
+
+            return BadRequest(new JsonResult(errorList));
+
+        }
+
+        // Login Method
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login([FromBody]LoginViewModel formdata)
+        {
+            // Get the User from database
+            var user = await _userManager.FindByNameAsync(formdata.Username);
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appSetting.Secret));
+
+            double tokenExpiryTime = Convert.ToDouble(_appSetting.ExpireTime);
+
+            if(user!=null && await _userManager.CheckPasswordAsync(user,formdata.Password))
+            {
+                // Confirmation of email 
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub,formdata.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                        new Claim(ClaimTypes.NameIdentifier,user.Id),
+                        new Claim(ClaimTypes.Role,roles.FirstOrDefault()),
+                        new Claim("LoggedOn",DateTime.Now.ToString()),
+                    }),
+                    SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature),
+                    Issuer = _appSetting.Site,
+                    Audience = _appSetting.Audience,
+                    Expires = DateTime.UtcNow.AddMinutes(tokenExpiryTime)
+                };
+
+                // Generate Token
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                return Ok(new { token = tokenHandler.WriteToken(token), expiration = token.ValidTo,
+                                username = user.UserName, useRole = roles.FirstOrDefault() });
+
+
+            }
+
+            // return error
+            ModelState.AddModelError("", "Username/Password was not found!");
+            return Unauthorized(new { LoginError = "Please Check the Login Creditials - Invalid Username/Password was enterd!" });
+        }
+
+        
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NG_Core_Auth.Data;
+using NG_Core_Auth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace NG_Core_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController:Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("[action]")]
+        [Authorize(Policy = "RequiredLoggedIn")]
+        public IActionResult GetProducts()
+        {
+
+            return Ok(_db.Products.ToList());
+        }
+
+
+        [HttpPost("[action]")]
+        [Authorize(Policy = "AdministratorRole")]
+        public async Task<IActionResult> AddProduct([FromBody] ProductModel formdata)
+        {
+            var newProduct = new ProductModel
+            {
+                Name = formdata.Name,
+                Description = formdata.Description,
+                ImageUrl = formdata.ImageUrl,
+                OutOfStock = formdata.OutOfStock,
+                Price = formdata.Price
+            };
+
+            await _db.Products.AddAsync(newProduct);
+
+            await _db.SaveChangesAsync();
+
+            return Ok(new JsonResult("The Product was Added Successfully!"));
+        }
+        
+        [HttpPut("[action]/{id}")]
+        [Authorize(Policy = "AdministratorRole")]
+        public async Task<IActionResult> UpdateProduct([FromRoute] int id,[FromBody] ProductModel formdata)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var findProduct = _db.Products.FirstOrDefault(p => p.ProductId == id);
+
+            if(findProduct==null)
+            {
+                return NotFound();
+            }
+
+            // If the product was found
+            findProduct.Name = formdata.Name;
+            findProduct.Description = formdata.Description;
+            findProduct.ImageUrl = formdata.ImageUrl;
+            findProduct.OutOfStock = formdata.OutOfStock;
+            findProduct.Price = formdata.Price;
+
+            _db.Entry(findProduct).State=EntityState.Modified;
+
+            await _db.SaveChangesAsync();
+
+            return Ok(new JsonResult("The Product with id " + id + " is updated"));
+
+        }
+
+        [HttpDelete("[action]/{id}")]
+        [Authorize(Policy = "AdministratorRole")]
+        public async Task<IActionResult> DeleteProduct([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // find the product
+            var findProduct = _db.Products.FirstOrDefault(p => p.ProductId == id);
+
+            if (findProduct == null)
+            {
+                return NotFound();
+            }
+
+            _db.Products.Remove(findProduct);
+
+            await _db.SaveChangesAsync();
+
+            return Ok(new JsonResult("The Product with id " + id + " is deleted"));
+
+        }
+        
+    }
+}
